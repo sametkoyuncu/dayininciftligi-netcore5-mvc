6081c759a680bc2cc798001beebe05bef9893cd9
sametkoyuncu <sametkoyuncu@live.com>
2021-01-11 14:12:25 +0300

    Add product pages for admin area

---
+++ DayininCiftligiNetCore5/Areas/Admin/ViewComponents/ProductCreateComponent.cs
@@ -0,0 +1,17 @@
+﻿using DayininCiftligiNetCore5.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayininCiftligiNetCore5.Areas.Admin.ViewComponents
+{
+    public class ProductCreateComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(new ProductModel());
+        }
+    }
+}
